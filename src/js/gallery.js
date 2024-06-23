fb02862bd24c7b57c7ce0591f1d5cfbdc74cdf06
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-06-24 00:24:57 +0100

    gallery fix

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,34 +1,34 @@
 const images = [
     {
-        url: '../img/gallery/no-name-face/135855.jpg',
+        url: '/img/gallery/no-name-face/135855.jpg',
         description: 'Jason Wade',
     },
     {
-         url: '../img/gallery/no-name-face/Yeah2.jpg',
+         url: '/img/gallery/no-name-face/Yeah2.jpg',
         description: 'Sergio, Jason and Rick',
     },
     {
-        url: '../img/gallery/no-name-face/Yeah3.jpg',
+        url: '/img/gallery/no-name-face/Yeah3.jpg',
         description: 'Jason, Sergio and Rick',
     },
     {
-         url: '../img/gallery/stanley-climbfall/promo.jpg',
+         url: '/img/gallery/stanley-climbfall/promo.jpg',
         description: 'Lifehouse on the truck',
     },
     {
-         url: '../img/gallery/stanley-climbfall/09-lifehouse-082007.jpg',
+         url: '/img/gallery/stanley-climbfall/09-lifehouse-082007.jpg',
         description: 'Jason, Sergio and brothers Woolstenhulme',
     },
     {
-         url: '../img/gallery/stanley-climbfall/Amsterdam12.jpg',
+         url: '/img/gallery/stanley-climbfall/Amsterdam12.jpg',
         description: 'Amsterdam',
     },
     {
-         url: '../img/gallery/stanley-climbfall/band055.jpg',
+         url: '/img/gallery/stanley-climbfall/band055.jpg',
         description: 'Photoshot',
     },
     {
-         url: '../img/gallery/stanley-climbfall/seanrick.jpg',
+         url: '/img/gallery/stanley-climbfall/seanrick.jpg',
         description: 'Sean and Rick',
     },
 ];
