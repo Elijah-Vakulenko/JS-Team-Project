cfeb94e1f042f5846fc51bbc5df8e2eff2b281ab
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-06-23 21:11:01 +0100

    Update gallery.js

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,35 +1,35 @@
 const images = [
     {
         url: '../img/gallery/no-name-face/135855.jpg',
-        description: '',
+        description: 'Jason Wade',
     },
     {
          url: '../img/gallery/no-name-face/Yeah2.jpg',
-        description: '',
+        description: 'Sergio, Jason and Rick',
     },
     {
         url: '../img/gallery/no-name-face/Yeah3.jpg',
-        description: '',
+        description: 'Jason, Sergio and Rick',
     },
     {
          url: '../img/gallery/stanley-climbfall/promo.jpg',
-        description: '',
+        description: 'Lifehouse on the truck',
     },
     {
          url: '../img/gallery/stanley-climbfall/09-lifehouse-082007.jpg',
-        description: '',
+        description: 'Jason, Sergio and brothers Woolstenhulme',
     },
     {
          url: '../img/gallery/stanley-climbfall/Amsterdam12.jpg',
-        description: '',
+        description: 'Amsterdam',
     },
     {
          url: '../img/gallery/stanley-climbfall/band055.jpg',
-        description: '',
+        description: 'Photoshot',
     },
     {
          url: '../img/gallery/stanley-climbfall/seanrick.jpg',
-        description: '',
+        description: 'Sean and Rick',
     },
 ];
 
