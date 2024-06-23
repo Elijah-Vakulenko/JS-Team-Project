7c9b0c16a4471190372266e3b21ab1073d5a4352
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-06-23 20:03:15 +0000

    Deploying to gh-pages from @ Elijah-Vakulenko/Lifehouse.ua@7a8d8d216bbcc4a233c0f40d04b4255e4266a356 🚀

--- assets/no-name-face-gallery-4a176b74.js
+++
@@ -1,7 +0,0 @@
-import{S as i}from"./vendor-5d6e4026.js";const a=[{url:"../img/gallery/no-name-face/135855.jpg",description:""},{url:"../img/gallery/no-name-face/Yeah2.jpg",description:""},{url:"../img/gallery/no-name-face/Yeah3.jpg",description:""},{url:"../img/gallery/stanley-climbfall/promo.jpg",description:""},{url:"../img/gallery/stanley-climbfall/09-lifehouse-082007.jpg",description:""},{url:"../img/gallery/stanley-climbfall/Amsterdam12.jpg",description:""},{url:"../img/gallery/stanley-climbfall/band055.jpg",description:""},{url:"../img/gallery/stanley-climbfall/seanrick.jpg",description:""}];let e=document.querySelector(".gallery");const r=a.map(function(l){return`
-  <li class="gallery-item">
-    <a class="gallery-link" href="${l.url}">
-      <img class="gallery-image" src="${l.url}" alt="${l.description}" title="${l.description}">
-    </a>
-  </li>`}),n=`${r.join("")}`;e.innerHTML=n;e=new i(".gallery a",{captionDelay:250});
-//# sourceMappingURL=no-name-face-gallery-4a176b74.js.map
