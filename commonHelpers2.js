8d19fe8d4b1b050469a814153c1d88754ec420cc
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-06-24 21:01:56 +0000

    Deploying to gh-pages from @ Elijah-Vakulenko/Lifehouse.ua@81f84886aeffbb9db0b933f90828ad7935cd43cd 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,7 +1,7 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{S as i}from"./assets/vendor-4bfe436b.js";const a=[{url:"/img/gallery/no-name-face/135855.jpg",description:"Jason Wade"},{url:"/img/gallery/no-name-face/Yeah2.jpg",description:"Sergio, Jason and Rick"},{url:"/img/gallery/no-name-face/Yeah3.jpg",description:"Jason, Sergio and Rick"},{url:"/img/gallery/stanley-climbfall/promo.jpg",description:"Lifehouse on the truck"},{url:"/img/gallery/stanley-climbfall/09-lifehouse-082007.jpg",description:"Jason, Sergio and brothers Woolstenhulme"},{url:"/img/gallery/stanley-climbfall/Amsterdam12.jpg",description:"Amsterdam"},{url:"/img/gallery/stanley-climbfall/band055.jpg",description:"Photoshot"},{url:"/img/gallery/stanley-climbfall/seanrick.jpg",description:"Sean and Rick"}];let e=document.querySelector(".gallery");const r=a.map(function(l){return`
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{S as s}from"./assets/vendor-4bfe436b.js";const a="/Lifehouse.ua/assets/135855-309185be.jpg",i="/Lifehouse.ua/assets/Yeah2-83e5a503.jpg",o="/Lifehouse.ua/assets/Yeah3-53ed440e.jpg",t="/Lifehouse.ua/assets/promo-9aa2a2e9.jpg",r="/Lifehouse.ua/assets/09-lifehouse-082007-eb80f01d.jpg",n="/Lifehouse.ua/assets/Amsterdam12-0c1f1725.jpg",l="/Lifehouse.ua/assets/band055-8f82f13e.jpg",c="/Lifehouse.ua/assets/seanrick-1bdedf42.jpg",u=[{url:a,description:"Jason Wade"},{url:i,description:"Sergio, Jason and Rick"},{url:o,description:"Jason, Sergio and Rick"},{url:t,description:"Lifehouse on the truck"},{url:r,description:"Jason, Sergio and brothers Woolstenhulme"},{url:n,description:"Amsterdam"},{url:l,description:"Photoshot"},{url:c,description:"Sean and Rick"}],g=document.querySelector(".gallery"),p=u.map(function(e){return`
   <li class="gallery-item">
-    <a class="gallery-link" href="${l.url}">
-      <img class="gallery-image" src="${l.url}" alt="${l.description}" title="${l.description}">
+    <a class="gallery-link" href="${e.url}">
+      <img class="gallery-image" src="${e.url}" alt="${e.description}" title="${e.description}">
     </a>
-  </li>`}),n=`${r.join("")}`;e.innerHTML=n;e=new i(".gallery a",{captionDelay:250});
+  </li>`}),m=`${p.join("")}`;g.innerHTML=m;new s(".gallery a",{captionDelay:250});
 //# sourceMappingURL=commonHelpers2.js.map
