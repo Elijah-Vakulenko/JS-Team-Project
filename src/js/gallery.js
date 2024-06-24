81f84886aeffbb9db0b933f90828ad7935cd43cd
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-06-24 22:01:37 +0100

    Update gallery.js

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,42 +1,48 @@
+import image1 from '../img/gallery/no-name-face/135855.jpg';
+import image2 from '../img/gallery/no-name-face/Yeah2.jpg';
+import image3 from '../img/gallery/no-name-face/Yeah3.jpg';
+import image4 from '../img/gallery/stanley-climbfall/promo.jpg';
+import image5 from '../img/gallery/stanley-climbfall/09-lifehouse-082007.jpg';
+import image6 from '../img/gallery/stanley-climbfall/Amsterdam12.jpg';
+import image7 from '../img/gallery/stanley-climbfall/band055.jpg';
+import image8 from '../img/gallery/stanley-climbfall/seanrick.jpg';
+
 const images = [
-    {
-        url: '/img/gallery/no-name-face/135855.jpg',
-        description: 'Jason Wade',
-    },
-    {
-         url: '/img/gallery/no-name-face/Yeah2.jpg',
-        description: 'Sergio, Jason and Rick',
-    },
-    {
-        url: '/img/gallery/no-name-face/Yeah3.jpg',
-        description: 'Jason, Sergio and Rick',
-    },
-    {
-         url: '/img/gallery/stanley-climbfall/promo.jpg',
-        description: 'Lifehouse on the truck',
-    },
-    {
-         url: '/img/gallery/stanley-climbfall/09-lifehouse-082007.jpg',
-        description: 'Jason, Sergio and brothers Woolstenhulme',
-    },
-    {
-         url: '/img/gallery/stanley-climbfall/Amsterdam12.jpg',
-        description: 'Amsterdam',
-    },
-    {
-         url: '/img/gallery/stanley-climbfall/band055.jpg',
-        description: 'Photoshot',
-    },
-    {
-         url: '/img/gallery/stanley-climbfall/seanrick.jpg',
-        description: 'Sean and Rick',
-    },
+  {
+    url: image1,
+    description: 'Jason Wade',
+  },
+  {
+    url: image2,
+    description: 'Sergio, Jason and Rick',
+  },
+  {
+    url: image3,
+    description: 'Jason, Sergio and Rick',
+  },
+  {
+    url: image4,
+    description: 'Lifehouse on the truck',
+  },
+  {
+    url: image5,
+    description: 'Jason, Sergio and brothers Woolstenhulme',
+  },
+  {
+    url: image6,
+    description: 'Amsterdam',
+  },
+  {
+    url: image7,
+    description: 'Photoshot',
+  },
+  {
+    url: image8,
+    description: 'Sean and Rick',
+  },
 ];
 
-let gallery = document.querySelector(".gallery");
-
-// import image from '../img/gallery/no-name-face/135855.jpg'
-// console.log(image);
+const gallery = document.querySelector(".gallery");
 
 const imagesImport = images.map(function (image) {
   return `
@@ -44,16 +50,15 @@ const imagesImport = images.map(function (image) {
     <a class="gallery-link" href="${image.url}">
       <img class="gallery-image" src="${image.url}" alt="${image.description}" title="${image.description}">
     </a>
-  </li>`
+  </li>`;
 });
 
 const imageCollection = `${imagesImport.join('')}`;
 gallery.innerHTML = imageCollection;
 
 import SimpleLightbox from 'simplelightbox';
-
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-gallery = new SimpleLightbox('.gallery a', {
+new SimpleLightbox('.gallery a', {
   captionDelay: 250,
 });
